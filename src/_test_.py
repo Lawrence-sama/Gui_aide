512ecb48a975e78674fabf3e38a3f3a70736d866
HOLO <3494643485@qq.com>
2022-10-23 21:39:15 +0800

    V0.1 	modified:   src/_test_.py

--- src/_test_.py
+++ src/_test_.py
@@ -2,7 +2,7 @@ import Gui_resp
  
 
 data = Gui_resp.shop_resp()
-print(data)
+
 
  
 
@@ -10,6 +10,6 @@ print(data)
 filename='test.txt'
 with open(filename,'w') as file_obj:
   file_obj.write(data)
-
+  print("success")
 
 # Gui_resp.data_preprocess(data)
